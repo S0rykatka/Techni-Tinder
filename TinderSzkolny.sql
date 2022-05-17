78a6be4af7cf1ef4401add38deb674dad107a2f6
S0rykatka <u22_beahan_waw@technischools.com>
2022-05-17 09:23:50 +0200

    Create TinderSzkolny.sql

---
+++ TinderSzkolny.sql
@@ -0,0 +1,39 @@
+USE master
+GO
+
+DROP DATABASE IF EXISTS TinderSzkolny
+GO
+
+CREATE DATABASE TinderSzkolny
+GO
+
+USE TinderSzkolny
+GO
+
+CREATE TABLE Uzytkownicy (
+PK_IdUzytkownika INT PRIMARY KEY IDENTITY,
+Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) >= 3),
+Nazwisko VARCHAR(50) NOT NULL CHECK(LEN(Nazwisko) >= 3),
+Wiek SMALLINT NOT NULL CHECK(Wiek >= 14),
+Klasa CHAR(2) NOT NULL,
+Opis TEXT NOT NULL,
+)
+
+CREATE TABLE Oceny (
+FK_IdOceniajacego INT REFERENCES Uzytkownicy(PK_IdUzytkownika),
+FK_IdOcenianego INT REFERENCES Uzytkownicy(PK_IdUzytkownika),
+Oceny VARCHAR(5) CHECK(Oceny IN('Lewy','Prawy'))
+)
+
+CREATE TABLE PlanLekcji (
+FK_IdPlanLekcji INT REFERENCES Uzytkownicy(PK_IdUzytkownika),
+DzienTyg VARCHAR(5) NOT NULL CHECK(DzienTyg IN('Pon','Wt','Sr','Czw','Pt')),
+Lekcja1 VARCHAR(20),
+Lekcja2 VARCHAR(20),
+Lekcja3 VARCHAR(20),
+Lekcja4 VARCHAR(20),
+Lekcja5 VARCHAR(20),
+Lekcja6 VARCHAR(20),
+Lekcja7 VARCHAR(20),
+Lekcja8 VARCHAR(20)
+)
