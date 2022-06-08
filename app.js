ca03b02c433ec4fd7736503bbb57c95fa36e5a50
S0rykatka <u22_beahan_waw@technischools.com>
2022-06-08 11:59:31 +0200

    No i działa

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+const path = require("path");
 const session = require('express-session');
 const logger = require('morgan');
 
